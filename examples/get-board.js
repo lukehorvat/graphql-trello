46cbda7ff9402775f4d6a69ff72edab5e034cb73
Luke Horvat <lukehorvat@gmail.com>
2017-02-02 22:12:17 +1100

    Implement board example

---
+++ examples/get-board.js
@@ -0,0 +1,52 @@
+import graphqlTrello from "../lib";
+
+let query = /* GraphQL */`
+  query($boardId: String!) {
+    getBoard(boardId: $boardId) {
+      id
+      name
+      url
+      lists {
+        id
+        name
+        cards {
+          id
+          name
+          url
+          updatedAt
+          comments
+          labels {
+            name
+            color
+          }
+          members {
+            id
+            username
+            fullName
+            avatarHash
+          }
+        }
+      }
+      members {
+        id
+        username
+        fullName
+        avatarHash
+      }
+    }
+  }
+`;
+
+graphqlTrello({
+  query,
+  variables: { boardId: process.env.TRELLO_BOARD_ID },
+  key: process.env.TRELLO_KEY,
+  token: process.env.TRELLO_TOKEN,
+}).then(data => {
+  let board = data.getBoard;
+  console.log(JSON.stringify(board, null, 2));
+  process.exit();
+}).catch(error => {
+  console.error(error);
+  process.exit(1);
+});
